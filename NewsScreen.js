06635bb03476aab460735e01e1cc237c59a97a7a
unknown <paul.solis@uniat.edu.mx>
2022-05-16 13:14:48 -0500

    tabs-drawers

---
+++ NewsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+function NewsScreen(props) {
+    return (
+        <View style = {{flex: 1,justifyContent: 'center', alignItems: 'center'}}>
+            <Text>
+                News
+            </Text>
+        </View>
+    );
+}
+
+export default NewsScreen;
---
+++ SettingsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+function SettingsScreen(props) {
+    return (
+        <View style = {{justifyContent: 'center', flex: 1, alignItems: 'center'}}>
+            <Text>
+                Settings
+            </Text>
+        </View>
+    );
+}
+
+export default SettingsScreen;
